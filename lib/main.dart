40d2d70d5320d813413730fe2d2f292b265da8bc
Dinesh Taral <dineshtaral@Dineshs-MacBook-Air.local>
2023-04-08 19:51:36 +0530

    restrict device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:movies_list_project/routes.dart';
 import 'package:movies_list_project/screens/movies_main_screen.dart';
 import 'package:movies_list_project/services/singleton_service.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(const MyApp());
 }
 
